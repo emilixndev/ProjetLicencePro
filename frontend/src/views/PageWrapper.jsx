7e33a8a93c72643e89382832598f7f408a9e2f1e
Yunus Cosar <yunuscosar67@gmail.com>
2023-01-30 13:25:21 +0100

    Tailwind + DaisyUI config

---
+++ frontend/src/views/PageWrapper.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Home";
+
+const PageWrapper = () => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default PageWrapper;
