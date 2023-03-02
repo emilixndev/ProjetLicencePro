bc3459b0a271007f090a9b5d38a19a20b48f5a12
Yunus Cosar <yunuscosar67@gmail.com>
2023-03-02 17:47:37 +0100

    react router init

--- frontend/src/views/PageWrapper.jsx
+++ frontend/src/views/PageWrapper.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Home from "./Home";
+import { Route, Routes } from "react-router-dom";
 
 const PageWrapper = () => {
   return (
     <div>
-      <Home></Home>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+      </Routes>
     </div>
   );
 };
