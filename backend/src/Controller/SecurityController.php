5c9ab379d69b2e2ac36c726d33bf826493621981
Emilixn <emilien.muck@gmail.com>
2023-03-13 11:06:12 +0100

    Reset password

--- backend/src/Controller/SecurityController.php
+++ backend/src/Controller/SecurityController.php
@@ -2,13 +2,26 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher,
+    )
+    {
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -28,5 +41,34 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+
+    #[Route('/resetUserPassword', name: 'resetUserPassword')]
+    public function resetUserPassword(Request $request){
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+        if(!password_verify($form->get('actualPassword')->getData(), $user->getPassword()) && $form->isSubmitted()){
+            $form->get('plainPassword')->get('first')->addError(new FormError("Le mot de passe actuel est invalide")); //'Le mot de passe actuel est invalide'
+            return $this->renderForm('security/formResetUserPassword.html.twig', [
+                'form' => $form,
+            ]);
+        }
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $this->getUser();
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            );
+            $user->setPassword($hashedPassword);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute("admin");
+        }
+        return $this->renderForm('security/formResetUserPassword.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
