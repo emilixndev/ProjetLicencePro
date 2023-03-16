31ba816deefdcff3f98e9b84a2d94ed5ed39364f
Emilixn <emilien.muck@gmail.com>
2023-03-16 17:11:12 +0100

    email

--- backend/src/Event/EventSubscriber/EntitySubscriber.php
+++ backend/src/Event/EventSubscriber/EntitySubscriber.php
@@ -5,10 +5,8 @@ namespace App\Event\EventSubscriber;
 
 use App\Entity\ImgMaterial;
 use App\Entity\Reservation;
-use App\Form\MaterialImgType;
 use App\Service\Emailservice;
 use Doctrine\Common\EventSubscriber;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -19,7 +17,6 @@ class EntitySubscriber implements EventSubscriber
 
     public function __construct(
         private Emailservice $emailservice,
-        private EntityManagerInterface $entityManager,
         private ParameterBagInterface $params
     )
     {
@@ -38,13 +35,8 @@ class EntitySubscriber implements EventSubscriber
     {
         $entity = $args->getObject();
         if($entity instanceof Reservation){
-            if(!$entity->getEndDate()){
-                $entity->getMaterial()->setIsAvailable(false);
-                $this->entityManager->persist($entity);
-                $this->entityManager->flush();
-            }
-//            $this->emailservice->sendConfirmationReservation($entity);
-//            $this->emailservice->sendNotificationOwner($entity);
+            $this->emailservice->sendConfirmationReservation($entity);
+            $this->emailservice->sendNotificationOwner($entity);
         }
 
 
